d413febc5f8aed7be49af195b8cdac70a9f639f8
Dennis Schulz <dennis@port42.org>
2014-11-30 21:37:38 +0100

    adds jenkins home volume

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name rd-jenkins -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) $1 rd-jenkins-image $2
+docker run --name rd-jenkins -p 8080:8080 -v /home/jenkins/home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) $1 rd-jenkins-image $2
