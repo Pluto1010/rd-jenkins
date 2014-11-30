c145726e67dce9615abfbd1263676969fda45a4d
Dennis Schulz <dennis@port42.org>
2014-11-30 21:31:51 +0100

    adds two parameters

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name rd-jenkins -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) rd-jenkins-image
+docker run --name rd-jenkins -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) $1 rd-jenkins-image $2
